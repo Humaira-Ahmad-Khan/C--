f9a3f9e67c8b7d8b1ba04477d915371b111f0ff8
Humaira <digitalartistctn@gmail.com>
2024-12-07 22:34:40 +0500

    Explicit

---
+++ 2nd_day/code.cpp
@@ -0,0 +1,40 @@
+//-> Constants
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     const float PI =  3.14 ;
+//     cout<<"PI = "<<PI<<endl;
+//     return 0;
+// }
+
+//->Typecasting
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     cout<<"Value 1 = "<<(10/3)<<endl;//output->3
+//     cout<<"Value 2 = "<<(10/3.0)<<endl;//output->3.3333
+//     return 0;
+// }
+
+//->Char and int = Integer (Improve)
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     cout<<"Value  = "<<('A' - 0)<<endl;//output->66(A = '65')
+//     cout<<"Value  = "<<('B' - 0)<<endl;//output->67(B = '66')
+//     cout<<"Value  = "<<('A')
+//     return 0;
+// }
+
+//Explicit
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Value  = "<<(int)('A')
+    return 0;
+}
