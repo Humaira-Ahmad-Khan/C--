ac64e81f7da4e04c931bc84f7688acbbc6409155
Humaira <digitalartistctn@gmail.com>
2024-12-09 20:48:53 +0500

    Namespace

--- 3rd_day/code.cpp
+++ 3rd_day/code.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
 using namespace std;
-int num = 100;
-int main(){
-    int num = 10;
-    // cout<<"Enter number = ";
-    // cin>>num;
-    cout<<"This is the value "<<::num<<endl;
+namespace first{
+    void show(){
+    cout<<"bobo"<<endl;
+}
+}
+namespace second{
+   void show(){
+    cout<<"humaira";
+}
+}
+
+
+
+ main(){
+    first::show();
+    second::show();
      
     return 0;
 }
+// int num = 100;
+// int main(){
+//     int num = 10;
+//     // cout<<"Enter number = ";
+//     // cin>>num;
+//     cout<<"This is the value "<<::num<<endl;
+     
+//     return 0;
+// }
