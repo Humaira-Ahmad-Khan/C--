94ff0dab1b68c94c8915b20dd2eb9670e095b2aa
Humaira <digitalartistctn@gmail.com>
2024-12-09 20:28:47 +0500

    Globle

---
+++ 3rd_day/code.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int num = 100;
+int main(){
+    int num = 10;
+    // cout<<"Enter number = ";
+    // cin>>num;
+    cout<<"This is the value "<<::num<<endl;
+     
+    return 0;
+}
--- oop/Student_class.cpp
+++ oop/Student_class.cpp
@@ -19,7 +19,7 @@ public:
         studentobject.Class = 8;
         studentobject.Gender = "Female";
         studentobject.RollNumber = 387;
-        cout <<"Student Name is "<<studentobject.Name<<endl;
+        cout <<"Student Name is "<<studentobject.Name<< endl;
         cout <<"Student Age is "<<studentobject.Age<< endl;
         cout <<"Student Gender is "<<studentobject.Gender<< endl;
         cout <<"Student RollNumber is "<<studentobject.RollNumber<<endl;
