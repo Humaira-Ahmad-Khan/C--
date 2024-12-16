13b9f410ac12279066636c492ad4adfe1d8e605a
Humaira <digitalartistctn@gmail.com>
2024-12-16 22:15:49 +0500

    Find Area

--- oop/FP.cpp
+++ oop/FP.cpp
@@ -3,14 +3,28 @@ using namespace std;
 // int a =  5;
 // int b = 10;
 // int Sum = a+b;
-void myFunction(string fname ,  int age )  {
-    cout<<fname<<" "<<"Hello!"<<" "<<"Age is"<<" "<<age<<endl;
+// void myFunction(string fname ,  int age )  {
+//     cout<<fname<<" "<<"Hello!"<<" "<<"Age is"<<" "<<age<<endl;
+
+
+// };
+// int main(){
+//     myFunction("Ahmad", 20);
+//     myFunction("BoBo", 18);
+//     myFunction("Humaira", 13);
+//     return 0;
+// }
+
+
+
+
+void area(int length,int width)  {
+    cout<<"length = "<<length  <<", width = "<<width<<", Area = "<<length * width <<endl;
 
 
 };
 int main(){
-    myFunction("Ahmad", 20);
-    myFunction("BoBo", 18);
-    myFunction("Humaira", 13);
+    area(10, 20);
+    area(12,8);
     return 0;
 }
